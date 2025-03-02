0c8aa8ff36eb220d09e5d4ed53685e1ef5340945
Navneet Singh <navneetsingh1825@gmail.com>
2025-03-02 20:28:33 +0530

    port updated

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/paymentRouter.js";
 // app config
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000 ;
 
 // middleware
 app.use(express.json());
